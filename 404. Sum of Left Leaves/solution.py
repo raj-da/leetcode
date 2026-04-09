87db5c70e67295b54df611c1dfaa67937ba5276a
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2026-04-09 12:18:49 +0300

    404. Sum of Left Leaves

---
+++ 404. Sum of Left Leaves/solution.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        
+        def dfs(node, dir):
+            if not node:
+                return 0
+            if (not node.left) and (not node.right):
+                return node.val if dir == 'l' else 0
+            
+            return dfs(node.left, 'l') + dfs(node.right, 'r')
+        
+        return dfs(root, '')
